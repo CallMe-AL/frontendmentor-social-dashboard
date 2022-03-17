13c876ab7a8e36261c9bdd70041ccc69a4f22ac6
unknown <aindovina2@yahoo.com>
2022-03-17 12:28:06 -0400

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const themeSwitch = document.getElementById('theme-switch');
 const body = document.querySelector('body');
 
+// TODO check to make sure theme stays same color on page refresh
+
 themeSwitch.addEventListener('change', () => {
   if (themeSwitch.checked) {
     body.classList.add('light-theme');
