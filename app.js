45943ae432707423ccfcfe4ca0c99b86cb6457f7
unknown <aindovina2@yahoo.com>
2022-03-18 12:09:08 -0400

    fixed iphone wrapping issue, added localstorage functionality for theme preferences

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const themeSwitch = document.getElementById('theme-switch');
 const body = document.querySelector('body');
 
-// TODO check to make sure theme stays same color on page refresh
+window.addEventListener('load', () => {
+  let currentTheme = localStorage.getItem('theme');
+
+  if (currentTheme === 'dark') {
+    body.classList.add('dark-theme');
+  } else {
+    body.classList.add('light-theme');
+  }
+
+});
 
 themeSwitch.addEventListener('change', () => {
   if (themeSwitch.checked) {
     body.classList.add('light-theme');
+    let theme = "light";
+    localStorage.setItem('theme', theme);
     body.classList.remove('dark-theme');
   } else {
     body.classList.remove('light-theme');
     body.classList.add('dark-theme');
+    let theme = "dark";
+    localStorage.setItem('theme', theme);
   }
 });
