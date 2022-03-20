1005c4173a0698c914acea1fec573776c4c22d23
unknown <aindovina2@yahoo.com>
2022-03-20 11:37:12 -0400

    theme switch fixes for localstorage

--- app.js
+++ app.js
@@ -5,19 +5,23 @@ window.addEventListener('load', () => {
   let currentTheme = localStorage.getItem('theme');
 
   if (currentTheme === 'dark') {
-    body.classList.add('dark-theme');
+    body.classList.remove('light-theme');
+    body.classList.add('dark-theme');    
+    themeSwitch.checked = false;
   } else {
-    body.classList.add('light-theme');
+    body.classList.remove('dark-theme');
+    body.classList.add('light-theme');    
+    themeSwitch.checked = true;
   }
 
 });
 
 themeSwitch.addEventListener('change', () => {
   if (themeSwitch.checked) {
+    body.classList.remove('dark-theme');
     body.classList.add('light-theme');
     let theme = "light";
-    localStorage.setItem('theme', theme);
-    body.classList.remove('dark-theme');
+    localStorage.setItem('theme', theme);    
   } else {
     body.classList.remove('light-theme');
     body.classList.add('dark-theme');
